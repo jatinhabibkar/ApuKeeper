20a83df4ebe9766783e000e75acea37c750d55af
jatinhabibkar <jatinkrishnahabibkar@gmail.com>
2024-08-24 16:24:10 +0530

    springboot init

---
+++ apukeeper/src/main/java/com/project/apukeeper/ApukeeperApplication.java
@@ -0,0 +1,13 @@
+package com.project.apukeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApukeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApukeeperApplication.class, args);
+	}
+
+}
---
+++ apukeeper/src/test/java/com/project/apukeeper/ApukeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apukeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApukeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
